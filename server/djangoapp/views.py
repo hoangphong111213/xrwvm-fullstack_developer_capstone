8b22ab7ee509dfa6eb1fd17d758b779ecce8b5af
hoangphong111213 <hoangphong1110ams@gmail.com>
2025-01-02 12:13:37 -0500

    SHIT PROJECT

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -91,12 +91,12 @@ def get_cars(request):
 # a list of dealerships
 #Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
-    if(state == "All"):
+    if state == "All":
         endpoint = "/fetchDealers"
     else:
-        endpoint = "/fetchDealers/"+state
+        endpoint = "/fetchDealers/" + state
     dealerships = get_request(endpoint)
-    return JsonResponse({"status":200,"dealers":dealerships})
+    return JsonResponse({"status": 200, "dealers": dealerships})
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 def get_dealer_reviews(request, dealer_id):
